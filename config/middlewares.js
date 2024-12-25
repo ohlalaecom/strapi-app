b59fd4f187b937bac7493da505fe5ad19c7e8f76
ohlalaecom <ecommerceolala@gmail.com>
2024-12-25 15:43:35 +0530

    middleware. modifications
    middleware. modifications

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,15 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://oh-lala.co'], // Replace with your frontend URL
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+      headers: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'], // Allowed headers
+      credentials: true, // Allow credentials if needed (e.g., cookies, Authorization header)
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
