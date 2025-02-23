9aa4f5c8f7caaa762fb20f5897c13157218039ed
ohlalaecom <ecommerceolala@gmail.com>
2025-02-23 18:26:47 +0530

    push latest
    push latest

--- config/database.js
+++ config/database.js
@@ -1,11 +1,27 @@
 const path = require('path');
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'db/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'db/data.db')),
+      host: env('DATABASE_HOST', 'your-render-host'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'your-database-name'),
+      user: env('DATABASE_USERNAME', 'your-username'),
+      password: env('DATABASE_PASSWORD', 'your-password'),
+      ssl: { rejectUnauthorized: false }, // Required for Render
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
+
