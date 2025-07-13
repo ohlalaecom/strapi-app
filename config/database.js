1069282d682127c158744205e386a9a2f5f541ac
ohlalaecom <ecommerceolala@gmail.com>
2025-07-14 02:02:07 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -14,14 +14,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'your-render-host'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'your-database-name'),
-      user: env('DATABASE_USERNAME', 'your-username'),
-      password: env('DATABASE_PASSWORD', 'your-password'),
-      ssl: { rejectUnauthorized: false }, // Required for Render
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: { rejectUnauthorized: false },
     },
     debug: false,
   },
 });
-
