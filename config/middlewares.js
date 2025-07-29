bd273e48c5f7b94c962e9222d19601bf6cc0f05e
ohlalaecom <ecommerceolala@gmail.com>
2025-07-30 02:24:31 +0530

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,10 +4,10 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: '*', // Allow any website or URL
-      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-      headers: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'], // Allowed headers
-      credentials: false, // Set to true only if needed
+      origin: ['http://localhost:3000', 'https://jacobs-electronics.com'], // ✅ Use specific origins
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+      credentials: true, // ✅ Set to true to allow cookies/auth headers
     },
   },
   'strapi::poweredBy',
